7704d765095f50a45a3d086b5a4ec0936e4566ba
jjmin321 <jjmin321@naver.com>
2019-11-26 18:05:02 +0900

    Update file

---
+++ syntax/Number.js
@@ -0,0 +1,4 @@
+console.log(1+1);
+console.log(2-2);
+console.log(3*3);
+console.log(4/2);
---
+++ syntax/String.js
@@ -0,0 +1,2 @@
+console.log('9'+'5');
+console.log('asoerhaipheriawhrpiahw39ahw3-rhwahcrosihdfaxhoefihapweifhpainfcianbvpiasdbgpashdgiphaspgpaidhgp'.length);
