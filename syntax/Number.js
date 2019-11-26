f3db54e0c815e691c01f5ee98268b1ce4b332ea1
jjmin321 <jjmin321@naver.com>
2019-11-27 08:39:31 +0900

    Patch

--- syntax/Number.js
+++
@@ -1,4 +0,0 @@
-console.log(1+1);
-console.log(2-2);
-console.log(3*3);
-console.log(4/2);
--- syntax/String.js
+++
@@ -1,2 +0,0 @@
-console.log('9'+'5');
-console.log('asoerhaipheriawhrpiahw39ahw3-rhwahcrosihdfaxhoefihapweifhpainfcianbvpiasdbgpashdgiphaspgpaidhgp'.length);
